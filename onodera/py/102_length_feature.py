e03d9feb56754c818fafe98dce8d9235d0afe571
kazuki-onodera <kazuki.onodera@dena.com>
2018-03-09 18:43:24 +0900

    korekara ganbaru

---
+++ onodera/py/102_length_feature.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+
+train = pd.read_pickle('../data/train2.p')
+test = pd.read_pickle('../data/test2.p')
+
+
+# length features
+def main(df):
+    
+    df['comment_len'] = df.comment_text.map(len)
+    comment_split = df.comment_text.map(lambda x: x.split())
+    df['word_cnt'] = comment_split.map(lambda x: len(x))
+    df['word_cnt_unq'] = comment_split.map(lambda x: len(set(x)) )
+    df['word_max_len'] = comment_split.map(lambda x: max(map(len, set(x))) )
+    
+    col = ['id', 'comment_len', 'word_cnt', 'word_cnt_unq', 'word_max_len']
+    return df[col]
+
+# ==============================================================
+# main
+# ==============================================================
+
+main(train).to_pickle('../data/102_train.p')
+main(test).to_pickle('../data/102_test.p')
+
